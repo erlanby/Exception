713e66cf3d21c85021e85f1b96fc660ac0fa8a09
erlanby <108142526+erlanby@users.noreply.github.com>
2022-08-03 21:58:12 +0300

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             Car car = new Car("Запорожец :)");
+            drive();
         } catch (Exception e) {
             e.printStackTrace();
         }
